1508da111bc34bed28d688890fbaecbd3cb6ca9f
Lakshmanan Chelliah <lakshmananmailbag@gmail.com>
2025-12-29 20:01:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,8 +18,17 @@ python manage.py load_initial_data --settings=veteran_project.render_settings
 echo "Seeding master data..."
 python manage.py seed_data --settings=veteran_project.render_settings
 
+echo "creating state_users data...."
+python manage.py seed_state_users --settings=veteran_project.render_settings
+
 echo "Creating accounts user..."
 python manage.py create_accounts_user --settings=veteran_project.render_settings
+
+echo "setting up the state_admin_permissions permissions ..."
+python manage.py setup_state_admin_permissions --settings=veteran_project.render_settings
+
+echo "fixing the state_permissions ..."
+python manage.py fix_state_permissions --settings=veteran_project.render_settings
 
 echo "Initializing RBAC..."
 python manage.py init_rbac --settings=veteran_project.render_settings
